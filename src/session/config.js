52a845368a1c6ffd1725a8e29111c5376e03cf37
Luara Mineiro <luaramineiro@gmail.com>
2020-04-25 19:16:40 -0300

    Delete config.js

--- src/session/config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret:
-    "a470feefcbadfd12bb1e58216509eb75e61c1d4f2cba872b92afea14d0e363c24a9db10c9127433bca29a0e91135d8e717bda409ed6d5ba1dd4c3b947767dcfa",
-  expiresIn: "14d",
-};
